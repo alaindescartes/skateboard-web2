5b56592e5a745c5768d5c05f154b89f4bb99139a
alaindescartesuwishema <ualaindescartes@gmail.com>
2025-01-04 08:38:05 -0700

    made changes to the add to cart button

--- src/app/build/components/AddToCartButton.tsx
+++ src/app/build/components/AddToCartButton.tsx
@@ -4,24 +4,27 @@ import React from "react";
 import { useCart } from "../context/CartContext";
 
 type AddToCartButtonProps = {
-    item: {
-        id: string;
-        name: string;
-        price: number;
-    };
+  item: {
+    id: string;
+    name: string;
+    price: number;
+  };
 };
 
 const AddToCartButton = ({ item }: AddToCartButtonProps) => {
-    const { addItem } = useCart();
+  const { addItem } = useCart();
 
-    return (
-        <button
-            onClick={() => addItem(item)}
-            className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-        >
-            Add to Cart
-        </button>
-    );
+  // Add default quantity of 1
+  const cartItem = { ...item, quantity: 1 };
+
+  return (
+    <button
+      onClick={() => addItem(cartItem)}
+      className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+    >
+      Add to Cart
+    </button>
+  );
 };
 
 export default AddToCartButton;
